fd7eceda47741fa203bcc246b1cd17781274868d
dainn <nguyendai060703@gmail.com>
2026-01-23 01:29:48 +0700

    fix: provider bugs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,7 +17,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    Provider.of<MessagesProvider>(context, listen: false).getMessages();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<MessagesProvider>(context, listen: false).getMessages();
+    });
   }
 
   @override
