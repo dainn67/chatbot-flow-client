33f90f9afa4e71726d4206be90990bb07d0ba2b4
dainn <nguyendai060703@gmail.com>
2026-01-22 17:52:44 +0700

    api

--- lib/configs/api_config.dart
+++ lib/configs/api_config.dart
@@ -1,4 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'https://chatbot-flow-server.abc-elearning.org';
+  static const String baseUrl = 'https://practice-test.abc-elearning.org';
+  // static const String baseUrl = 'https://chatbot-flow-server.abc-elearning.org';
   // static const String baseUrl = 'http://localhost:8000';
 }
