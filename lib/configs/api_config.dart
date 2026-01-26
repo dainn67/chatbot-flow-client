b5d95571a53948a44bcea656bc972ab31134ae60
dainn <nguyendai060703@gmail.com>
2026-01-26 15:17:44 +0700

    minor changes

--- lib/configs/api_config.dart
+++ lib/configs/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const bool isABC = false;
+  static const bool isABC = true;
   static const String baseUrl = isABC ? 'https://practice-test.abc-elearning.org' : 'https://3dcad279c0b4.ngrok-free.app';
   // static const String baseUrl = 'https://chatbot-flow-server.abc-elearning.org';
   // static const String baseUrl = 'http://localhost:8000';
